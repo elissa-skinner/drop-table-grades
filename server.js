c105e3af62659762f99e2d36c1a957163262a6d8
Elissa Skinner <elissa_skinner1@baylor.edu>
2019-11-15 15:37:38 -0600

    trying to get website working

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-var http = require('http');
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+/*var http = require('http');
 var fs = require('fs');
 
 const PORT=8080; 
@@ -12,4 +26,4 @@ fs.readFile('./index.html', function (err, html) {
         response.write(html);  
         response.end();  
     }).listen(PORT);
-});
+});*/
