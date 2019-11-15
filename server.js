2317eaff9299b0601fecadb5edb6950a97572938
Elissa Skinner <elissa_skinner1@baylor.edu>
2019-11-15 15:53:58 -0600

    let's try github

--- server.js
+++ server.js
@@ -10,3 +10,20 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+/*var http = require('http');
+var fs = require('fs');
+
+const PORT=8080; 
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});*/
